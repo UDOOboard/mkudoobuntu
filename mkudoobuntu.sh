c290d03fe8951269a32f9ca5bc43c43d579d309a
Francesco Montefoschi <francesco.monte@gmail.com>
2015-11-10 01:53:35 +0100

    Added missing package

--- mkudoobuntu.sh
+++ mkudoobuntu.sh
@@ -36,7 +36,7 @@ WALLPAPER_DEF=UDOO-blue
 
 HOST_PACKAGES=( debootstrap qemu-user-static apt-cacher-ng rsync )
 
-BASE_PACKAGES=( openssh-server alsa-utils bash-completion 
+BASE_PACKAGES=( openssh-server alsa-utils bash-completion policykit-1
   bluez blueman curl dosfstools fbset iw nano module-init-tools ntp unzip usbutils 
   vlan wireless-tools wget wpasupplicant unicode-data console-data console-common 
   pv sysfsutils cpufrequtils ntfs-3g locate command-not-found man-db git i2c-tools 
